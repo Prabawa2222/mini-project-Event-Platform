6cac7d6cdc7a1f2a91a19f001b97b716a6085e3d
Arief Prabawa <ariefprabawa95@gmail.com>
2025-01-29 10:45:55 +0700

    fix:search and slug events

--- backend/src/routes/eventRoutes.ts
+++ backend/src/routes/eventRoutes.ts
@@ -4,11 +4,15 @@ import { EventController } from "../controllers/eventController";
 const router = Router();
 const events = new EventController();
 
+router.get("/search", async (req, res) => {
+  await events.searchEvents(req, res);
+}); // Search events
 router.get("/", (req, res) => events.getEvents(req, res)); // Home page
 router.get("/:slug", (req, res) => events.getEventBySlug(req, res)); // Event details
 router.post("/", (req, res) => events.createEvent(req, res)); // Create event
 router.patch("/:slug", (req, res) => events.updateEvent(req, res)); // Update event
-// router.delete("/:slug", (req, res) => events.softDeleteEvent(req, res)); // Soft delete event
-// router.get("/search", (req, res) => events.searchEvents(req, res)); // Search events
+router.delete("/:slug", async (req, res) => {
+  await events.softDeleteEvent(req, res);
+}); // Soft delete event
 
 export default router;
